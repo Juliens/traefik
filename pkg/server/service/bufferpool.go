2aab1ba286ac1944213fb185460733be8f871f10
Julien Salleyron <julien.salleyron@gmail.com>
2021-05-11 10:52:48 +0200

    Change bufferpool implem.

--- pkg/server/service/bufferpool.go
+++ pkg/server/service/bufferpool.go
@@ -1,17 +1,48 @@
 package service
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 const bufferPoolSize = 32 * 1024
 
-func newBufferPool() *bufferPool {
+func newBufferPoolOld() *bufferPool {
 	return &bufferPool{
 		pool: sync.Pool{
 			New: func() interface{} {
+				fmt.Println("ALLOC")
 				return make([]byte, bufferPoolSize)
 			},
 		},
 	}
+}
+
+func newBufferPool() *myAlloc {
+	return &myAlloc{}
+}
+
+type myAlloc struct {
+	pool [][]byte
+	lock sync.Mutex
+}
+
+func (a *myAlloc) Get() []byte {
+	var x []byte
+	a.lock.Lock()
+	if i := len(a.pool) - 1; i >= 0 {
+		x = a.pool[i]
+		a.pool = a.pool[:i]
+	} else {
+		x = make([]byte, bufferPoolSize)
+	}
+	a.lock.Unlock()
+	return x
+}
+func (a *myAlloc) Put(x []byte) {
+	a.lock.Lock()
+	a.pool = append(a.pool, x)
+	a.lock.Unlock()
 }
 
 type bufferPool struct {
